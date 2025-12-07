a9a9ea29b4de4b6355accf22563bd441ace65ecd
Taanveer22 <taanveer469@gmail.com>
2025-12-07 10:07:48 +0600

    alhamdulliah register user in register page and manage user login data save to firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,67 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const { createNewUser, setUser } = useContext(AuthContext);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+    console.log(form);
+    const name = form.get("name");
+    const photoUrl = form.get("photo-url");
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log({ name, photoUrl, email, password });
+
+    createNewUser(email, password)
+      .then((result) => {
+        const userData = result.user;
+        setUser(userData);
+        // console.log(userData);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="card bg-base-100 w-full max-w-md shrink-0">
-        <form className="card-body">
+        <form onSubmit={handleFormSubmit} className="card-body">
           <fieldset className="fieldset">
             <label className="label">Name</label>
-            <input type="text" className="input" placeholder="Name" />
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Name"
+            />
             <label className="label">Photo Url</label>
-            <input type="text" className="input" placeholder="Photo Url" />
+            <input
+              name="photo-url"
+              type="text"
+              className="input"
+              placeholder="Photo Url"
+            />
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+            />
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+            />
             <button className="btn btn-neutral mt-4">Register</button>
           </fieldset>
         </form>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,17 +1,44 @@
-import { createContext, useState } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 // ====== step 01 (create context) ===========
 const AuthContext = createContext();
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "taanveer",
-    email: "tv469@gmail.com",
-  });
+  // const [user, setUser] = useState({
+  //   name: "taanveer",
+  //   email: "tv469@gmail.com",
+  // });
+
+  const [user, setUser] = useState(null);
+  console.log(user);
+
+  const createNewUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
   const AuthInfo = {
     user,
     setUser,
+    createNewUser,
   };
+
+  //=============== get the currently sign in user =================
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   // ========= step 03 (add return provider) ===========
   return (
     <AuthContext.Provider value={AuthInfo}>{children}</AuthContext.Provider>
